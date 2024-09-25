4194984785e9f2ccca6d06b1b181e27d2b2724c3
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-09-25 22:13:34 +0530

    Calculator program

---
+++ Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class Calculator
+    {
+        public void Cal(int a, int b)
+        {
+            Console.WriteLine($"Addition is:{ a+b}");
+        }
+        public void Cal(double a, int b)
+        {
+            Console.WriteLine($"Multiplication is:{a * b}");
+        }
+        public void Cal(float a, int b)
+        {
+            Console.WriteLine($"Division is:{a / b}");
+        }
+        public void Cal(int a, float b)
+        {
+            Console.WriteLine($"Substraction is:{a - b}");
+        }
+    }
+    class Clscalculator
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal1 = new Calculator();
+            cal1.Cal(12,10);
+            cal1.Cal(1233d,10);
+            cal1.Cal(10.5F,3);
+            cal1.Cal(47,12.5F);
+
+        }
+    }
+}
---
+++ ClsAnimal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    class Animal
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+        }
+    }
+
+    class Circle : Animal
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+
+        }
+    }
+
+    class Rectangle : Animal
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+        }
+        
+
+    }
+    
+    internal class ClsAnimal
+    {
+        static void Main(string[] args)
+        {
+            Animal ani=new Animal();
+            ani.Draw();
+
+            Circle cir=new Circle();
+            cir.Draw();
+
+            Rectangle rect=new Rectangle();
+            rect.Draw();
+
+            Console.Read();
+
+        }
+    }
+}
