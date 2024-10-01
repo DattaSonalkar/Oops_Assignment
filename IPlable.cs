02374e73c34da53549d12406e1aff878970cb0b8
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-01 12:44:44 +0530

    interface ex

---
+++ IPlable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal interface IPlable
+    {
+        void Play();
+    }
+
+    class VideoPlayer:IPlable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Video");
+        }
+
+    }
+
+    class MusicPlayer:IPlable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Music");
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+                VideoPlayer p = new VideoPlayer();
+                p.Play();
+                MusicPlayer m=new MusicPlayer();
+                m.Play();
+
+                Console.Read();
+        }
+    }
+}
