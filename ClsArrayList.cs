cf9adfad6742aa7b28769f424e04595ff7755c07
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-02 14:51:55 +0530

    Arraylist and Hashtable

---
+++ ClsArrayList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class ClsArrayList
+    {
+        static void Main(string[] args)
+        {
+                ArrayList al = new ArrayList();
+                al.Add(1);
+            al.Add(2);
+            al.Add(3);
+            al.Add(4);
+            al.Add(5);
+            al.Add(6);
+            al.Add(7);
+            al.Add(8);
+            al.Add(9);
+            al.Add(10);
+
+            foreach (int i in al)
+            {
+                Console.Write(i +" ");
+            }
+
+            al.Remove(3);
+            Console.WriteLine();
+
+            foreach (int i in al)
+            {
+                Console.Write( i + " ");
+            }
+
+            al.Reverse();
+            Console.WriteLine();
+            foreach (int i in al)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ ClsHashTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class ClsHashTable
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("a", 1);
+            ht.Add("b", 2);
+            ht.Add("c", 3);
+            ht.Add("d", 4);
+            ht.Add("e", 5);
+
+            foreach (DictionaryEntry d in ht)
+            {
+                Console.Write(d.Key+" ");
+            }
+
+            Console.WriteLine();
+            ht.Remove("c");
+
+            foreach (DictionaryEntry d in ht)
+            {
+                Console.Write(d.Key +" ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
