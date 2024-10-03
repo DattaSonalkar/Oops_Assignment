0e590e8a1087a3eb5c9b677e92a7b26002253512
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-03 12:52:23 +0530

    Stack and Dictionary

---
+++ ClsDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class ClsDictionary
+    {
+        static void Main(string[] args)
+        {
+                Dictionary<int,string> d = new Dictionary<int,string>();
+            d.Add(101,"Datta");
+            d.Add(102, "Raj");
+            d.Add(103, "Vikas");
+            d.Add(104, "Shyam");
+            d.Add(105, "Anil");
+            d.Add(106, "Raju");
+            d.Add(107, "Ajit");
+            d.Add(108, "Dipak");
+            d.Add(109, "Ram");
+            d.Add(110, "Sachin");
+            foreach (KeyValuePair<int, string> kvp in d)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+            d[4] = "Rutu";
+
+            Console.WriteLine("Modified Values");
+            foreach (KeyValuePair<int, string> kvp in d)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+            d.Remove(6);
+
+            Console.WriteLine("After Removing Element");
+            foreach (KeyValuePair<int, string> kvp in d)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+
+            var filteredElements = d.Where(kvp => kvp.Value.Contains("Modified"));
+
+            
+            Console.WriteLine("Filtered elements:");
+            foreach (var kvp in filteredElements)
+            {
+                Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+
+        }
+    }
+}
---
+++ ClsStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Oops_Assignment
+{
+    internal class ClsStack
+    {
+        static void Main(string[] args)
+        {
+                Stack s =new Stack();
+                s.Push(1);
+                s.Push(2);
+                s.Push(3);
+                s.Push(4);
+                s.Push(5);
+                Console.WriteLine("Stack Elements are:");
+                foreach (var i in s) 
+                Console.WriteLine(i);
+
+                object se=s.Pop();
+                Console.WriteLine("Stack Elements After removing are:");
+                foreach (var i in s)
+                Console.WriteLine(i);
+
+                Console.Read();
+
+        }
+
+    }
+}
