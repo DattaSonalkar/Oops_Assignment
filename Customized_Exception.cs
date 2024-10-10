a478b5d7b8e5ab78267a2110235cc99559198acf
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-10 09:50:12 +0530

    Exception handling

---
+++ Customized_Exception.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Oops_Assignment
+{
+    class Negetive_Number_Exception:Exception
+    {
+        public Negetive_Number_Exception(string msg): base("Negative numbers are not allowed.")
+        {
+        }
+    }
+    internal class Customized_Exception
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter Any No:");
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num < 0)
+                {
+                    throw new Negetive_Number_Exception($"Error: {num} is a negative number.");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is a valid positive number.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
---
+++ Exception_handling.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    class Exception_handling
+    {
+        static void Main(string[] args)
+        {
+            int a=12, b=6, c=0;
+
+            try
+            {
+                c = a + b;
+                Console.WriteLine("Addition is:" + c);
+
+                c = a - b;
+                Console.WriteLine("Substraction is:" + c);
+
+                c = a * b;
+            }
+            catch 
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            Console.WriteLine("Multiplication is:"+c);
+
+            //Dividebyzeroexception
+            try
+            {
+                c = a / b;
+                Console.WriteLine("Division is:" + c);
+            }
+            catch(DivideByZeroException de) 
+            {
+                Console.WriteLine(de.Message);
+            }
+
+            //FormatException
+            try
+            {
+                string s = "abc123";
+                int result = int.Parse(s);  
+            }
+
+            catch(FormatException fe) 
+            {
+                Console.WriteLine(fe.Message);
+            }
+
+            //OverflowException
+            try
+            {
+                
+                int max = int.MaxValue;
+                int result = checked(max + 1);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+
+    }
+}
