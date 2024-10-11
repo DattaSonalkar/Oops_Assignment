ab95cbabd522286b09a48aaf338891fbb1f7476a
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-11 10:16:32 +0530

    Delegate Assignment

---
+++ Cls_Delegate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+
+    public delegate void Cls_Delegate(int a, int b);
+    public delegate void Delegate_Substraction(int a, int b);
+    class Cls_Delegate1
+    {
+        public void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition is:" + (a + b));
+        }
+
+        public void Substraction(int a, int b)
+        {
+            Console.WriteLine($"Substraction is :{a - b}");
+        }
+    }
+
+    class Cls_Delegate2
+    { 
+
+        static void Main()
+        {
+            //Delegate_Addition da = new Delegate_Addition(Display);
+            Cls_Delegate1 cd = new Cls_Delegate1();
+            Cls_Delegate cd1 = new Cls_Delegate(cd.Addition);
+            cd1(10,12);
+
+            Delegate_Substraction ds=new Delegate_Substraction(cd.Substraction);
+            ds(53,43);
+        }
+        
+    }
+}
---
+++ Multicast_Delegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    public delegate void MyDelegate(int x,int y);
+    class Multicast1
+    {
+        public void Add(int x,int y)
+        {
+            Console.WriteLine($"Add is:{x+y}");
+        }
+
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Sub is:{x - y}");
+        }
+
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine($"Mul is:{x * y}");
+        }
+    }
+    internal class Multicast_Delegate
+    {
+        static void Main(string[] args)
+        {
+                Multicast1 m1= new Multicast1();
+                MyDelegate md=new MyDelegate(m1.Add);
+                //md += m1.Add;
+                md += m1.Sub;
+                md += m1.Mul;
+                md(64,16);
+        }
+    }
+}
