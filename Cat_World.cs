e58f511160b80cb331bfadf3be6ddfe4b2688f4f
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-17 09:39:43 +0530

    Static and Sealed class

---
+++ Cat_World.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    public static class cls_Static
+    {
+        public static int NumberOfCats { get; private set; } = 0;
+        public static int TotalCatNaps { get; private set; } = 0;
+
+       public static void AddCat()
+        { 
+            NumberOfCats++;
+            Console.WriteLine($"A new cat joined Total cats: {NumberOfCats}");
+        }
+
+        public static void AddCatNaps(int naps)
+        {
+            TotalCatNaps += naps;
+            Console.WriteLine($"Added {naps} naps. Total naps: {TotalCatNaps}");
+        }
+
+    }
+    internal class Cat_World
+    {
+        static void Main(string[] args)
+        {
+            cls_Static.AddCat();
+            cls_Static.AddCat();
+
+            cls_Static.AddCatNaps(5);
+            cls_Static.AddCatNaps(3);
+
+        }
+    }
+}
---
+++ Sealed_Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    class Sealed_Class1
+    {
+        public void Display1()
+        {
+            Console.WriteLine("This is Base class");
+        }
+    }
+
+    sealed class Sealed_Class2:Sealed_Class1
+    {
+        public void Display2()
+        {
+            Console.WriteLine("This is derived class with Sealed");
+        }
+
+    }
+    class Sealed_Class
+    {
+        static void Main(string[] args)
+        {
+            Sealed_Class2 sc = new Sealed_Class2();
+            sc.Display1();
+            sc.Display2();
+        }
+    }
+}
