bac6fdec24b2d692f0ad4d6bf4357035b234d8ce
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-10-18 14:39:38 +0530

    ref and out keyword

---
+++ Out_Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class Out_Class
+    {
+        public static void Initilize_Values(int a,string s,out int rollno,out string name)
+        {
+            rollno = a;
+            name = s;
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string s = "Datta";
+            int rollno;
+            string name;
+            Initilize_Values(a,s,out rollno,out name);
+            Console.WriteLine($"Rollno is:{rollno}");
+            Console.WriteLine($"Name is:{name}");
+
+        }
+    }
+}
---
+++ Ref_Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Assignment
+{
+    internal class Ref_Class
+    {
+        public static void Increment_Value(ref int a)
+        {
+            a++;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine($"Vale Before Increment:{a}");
+            Increment_Value(ref a);
+            Console.WriteLine($"Value after increment:{a}");
+            Console.Read();
+            
+        }
+
+    }
+}
